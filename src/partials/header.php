c5e2eb6a11727943fc83c83b80428040aecd47d9
evo2000 <evancmurphy08@gmail.com>
2026-07-11 19:29:57 -0400

    add project-menu page

--- src/partials/header.php
+++ src/partials/header.php
@@ -1,12 +1,12 @@
 <header>
     <div class="title-bar">
-        <script type="text/processing" src="/src/assets/processing/proc_point.pde"></script>
+        <script type="text/processing" src="/assets/processing/proc_point.pde"></script>
         <canvas></canvas>
         <h1>techne.</h1>
     </div>
 
     <div class="navbar">
-        <a href="/about/">about</a>
-        <a href="/engineering/">engineering</a>
+        <a href="/about">about</a>
+        <a href="/projects">projects</a>
     </div>
 </header>
